d611690db63e26af0174878bcabe80edae6988c7
Huy Vo <huyvd250797@gmail.com>
2024-07-25 00:11:22 +0700

    JS: Build Product Manager app - building UI - 50%

---
+++ JS-function/function.js
@@ -0,0 +1,14 @@
+// //! Dùng preventDefault() cho input type="submit"
+
+// const preventDefaultItem = (iditem) => {
+//   document
+//     .getElementById(`"${iditem}"`)
+//     .addEventListener("click", function (e) {
+//       e.preventDefault();
+//     });
+// };
+
+// hàm Create new product
+const createNewProduct = () => {
+  console.log("yes");
+};
