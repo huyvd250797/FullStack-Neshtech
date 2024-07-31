e80483c63684cee33769439f69ccbab116ca6deb
Huy Vo <huyvd250797@gmail.com>
2024-07-31 23:39:56 +0700

    JS: Build PFM - 50%

--- JS-function/function.js
+++ JS-function/function.js
@@ -8,26 +8,26 @@
 //     });
 // };
 
-// hàm change format date
-function changedateformat(val) {
-  const myArray = val.split("-");
+// hàm change format date to DD/MM/YYYY
+const changedateformat = (date) => {
+  let dateFormated = date.split("-").reverse().join("/");
+  return dateFormated;
+};
 
-  let year = myArray[0];
-  let month = myArray[1];
-  let day = myArray[2];
-
-  let formatteddate = day + "-" + month + "-" + year;
-
-  return formatteddate;
-}
+// hàm clear all data add new
+const clearDataAddNew = () => {
+  document.getElementById("addNewExpenseDate").value = "";
+  document.getElementById("radioSpending").checked = false;
+  document.getElementById("radioIncome").checked = false;
+  document.getElementById("addNewTypeSpending").value = "";
+  document.getElementById("addNewGroupItem").value = "";
+  document.getElementById("addNewAmount").value = "";
+  document.getElementById("addNewDescription").value = "";
+  document.getElementById("addNewNote").value = "";
+};
 
 //* hàm color picker
 const getColorPicker = () => {
-  let getBgColor = document.getElementById("iconColorBgPicker").value;
-  let getTextColor = document.getElementById("iconColorTextPicker").value;
-  let backgroundColorIcon = document.getElementById("iconGroupItem");
-  let colorIcon = document.getElementById("icon-group-item");
-
   // Gán set background color theo màu đã chọn
   backgroundColorIcon.style.backgroundColor = getBgColor;
 
@@ -111,7 +111,69 @@ const chooseTypeSpending = () => {
 /* -------------------------- hàm group item ------------------------- */
 const getGroupItem = () => {
   let groupItemVal = document.getElementById("addNewGroupItem").value;
-  console.log(groupItemVal);
+  return groupItemVal;
+};
+
+//* ----------------- hàm khai báo list danh sách group item ----------------- */
+// thêm mới item ở đây
+const listGroupItem = () => {
+  let listItem = [
+    {
+      id: 1,
+      value: "Debt Payment",
+    },
+    {
+      id: 2,
+      value: "Drink",
+    },
+    {
+      id: 3,
+      value: "Entertain",
+    },
+    {
+      id: 4,
+      value: "Food",
+    },
+    {
+      id: 5,
+      value: "Gasoline",
+    },
+    {
+      id: 6,
+      value: "Salary",
+    },
+    {
+      id: 7,
+      value: "Saving",
+    },
+    {
+      id: 8,
+      value: "Invest",
+    },
+  ];
+
+  return listItem;
+};
+
+// hàm create Group Item
+const createGroupItem = () => {
+  // get list group item
+  let listGroupItems = listGroupItem();
+
+  let getListSelect = document.getElementById("addNewGroupItem");
+
+  // Khai báo biến lấy giá trị Object
+  let getValuesObject;
+
+  for (let i = 0; i < listGroupItems.length; i++) {
+    // Lấy giá trị Object bao gồm [2, "Drink"]
+    getValuesObject = Object.values(listGroupItems[i]);
+
+    // innerHTML giá trị đó vào option
+    getListSelect.innerHTML += `
+      <option value="${i + 1}">${getValuesObject[1]}</option>
+    `;
+  }
 };
 
 //*TODO ------------------------- hàm Add New ------------------------- */
@@ -119,14 +181,45 @@ const createNewExpense = () => {
   let getType = chooseType();
   let getTypeSpending = chooseTypeSpending();
 
+  // Khai báo biến lấy list groupItem
+  let getListGroupItem = listGroupItem();
+
   // Get value Group item
-  getGroupItem();
+  let getSelectedItem = getGroupItem();
+
+  console.log(getSelectedItem);
+
+  // Khai báo biến get value Object
+  let getValueObjItem;
+
+  // Khai báo biến get value item
+  let getValueItem;
+
+  // Khởi chạy i từ 1, vì nếu chạy từ 0 khi đến line 202 thì 0 - 1 sẽ âm ==> Không lấy dc giá trị
+  for (let i = 1; i <= getListGroupItem.length; i++) {
+    if (getSelectedItem == i) {
+      // Lấy values của Object ==> lấy được mảng gồm [7, "Saving"]
+      getValueObjItem = Object.values(getListGroupItem[i - 1]);
+      console.log(getValueObjItem);
+      // gán biến value item = value được lấy từ mảng trong object
+      //?? [7, "Saving"] ==> [1] sẽ là giá trị "Saving"
+      getValueItem = getValueObjItem[1];
+      break;
+    } else {
+      getValueItem = "";
+    }
+  }
 
   let addNewDate = document.getElementById("addNewExpenseDate").value;
+
+  // change YYYY-MM-DD --> DD/MM/YYYY
+  addNewDate = changedateformat(addNewDate);
+
   let addNewGroupItem = document.getElementById("addNewGroupItem").value;
   let addNewAmount = document.getElementById("addNewAmount").value;
   let addNewDescription = document.getElementById("addNewDescription").value;
   let addNewNote = document.getElementById("addNewNote").value;
+
   let getTableBody = document.getElementById("table-body");
 
   if (addNewDate.length == 0) {
@@ -142,6 +235,15 @@ const createNewExpense = () => {
   } else if (addNewDescription.length == 0) {
     alert("Enter Description Expense");
   } else {
+    document.getElementById("addNewExpenseModal");
+
+    //*!!! ------------------------------- START: HIDE MODAL ------------------------------- */
+    //  Khai báo biến lấy element modal
+    let getModalAddNew = document.getElementById("addNewExpenseModal");
+    let modal = bootstrap.Modal.getInstance(getModalAddNew);
+    modal.hide();
+    //*!!! ------------------------------- END: HIDE MODAL ------------------------------- */
+
     // Tạo thêm 1 row dữ liệu mới
     getTableBody.innerHTML += `
   <tr class="expense-item item-1">
@@ -155,7 +257,7 @@ const createNewExpense = () => {
       <span>${getTypeSpending}</span>
     </td>
     <td>${addNewAmount} đ</td>
-    <td>${addNewGroupItem}</td>
+    <td>${getValueItem}</td>
     <td>${addNewDescription}</td>
     <td>X.XXX.XXX đ</td>
     <td>${addNewNote}</td>
@@ -171,6 +273,8 @@ const createNewExpense = () => {
     </td>
   </tr>
   `;
+    // Sau khi lưu thành công clear all data in addnew
+    clearDataAddNew();
   }
 };
 
